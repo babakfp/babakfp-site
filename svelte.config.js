248712eceb8851282bacb4233ed4787b21f8595d
babakfp <44144724+babakfp@users.noreply.github.com>
2022-12-11 19:48:43 -0800

    got rid of @sveltejs/adapter-auto because it sucks

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess'
 import { mdsvex, escapeSvelte } from 'mdsvex'
 import { getHighlighter } from 'shiki'
