b16e6bc13c582ef431b4140f7d4d15041ea78eb6
babakfp <44144724+babakfp@users.noreply.github.com>
2022-12-13 02:35:52 -0800

    added a custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores'
+</script>
+
+<div class="grid justify-center text-center">
+	<h1 class="text-2xl font-semibold">{$page.status}</h1>
+	<p class="mt-2">{$page.error.message}</p>
+</div>
