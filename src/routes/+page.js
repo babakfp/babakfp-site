1217bf2d0848a8d0dd74cda82da513c92276fc2b
babakfp <44144724+babakfp@users.noreply.github.com>
2022-12-13 02:25:01 -0800

    error handing github fetch

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,10 +1,14 @@
+import { error } from '@sveltejs/kit'
 const pinnedRepos = ['svelte-outclick', 'tailwindcss-addons', 'farsgamer']
 
 export async function load({ fetch }) {
-	const repos = await Promise.all([
-		fetch('https://api.github.com/repos/babakfp/svelte-outclick').then(res => res.json()),
-		fetch('https://api.github.com/repos/babakfp/tailwindcss-addons').then(res => res.json()),
-	])
-
-	return { repos }
+	try {
+		const repos = await Promise.all([
+			fetch('https://api.github.com/repos/babakfp/svelte-outclick').then(res => res.json()),
+			fetch('https://api.github.com/repos/babakfp/tailwindcss-addons').then(res => res.json()),
+		])
+		return { repos }
+	} catch (_) {
+		throw error(500, 'Unsuccessful fetching the data from GitHub!')
+	}
 }
