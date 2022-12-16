17fde2cd8703481476de4e4c1fa03e31fe6f1beb
babakfp <44144724+babakfp@users.noreply.github.com>
2022-12-16 08:43:43 -0800

    added label

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -89,7 +89,7 @@
 		</Link>
 		<Link href="https://elemen.ir">Store</Link>
 		<button
-			class="inline-flex gap-2 py-2 px-3 pr-4 rounded text-sm bg-gray-800/50 backdrop-blur highlight-white/5 select-none duration-150 hover:bg-gray-700/50"
+			class="group relative inline-flex gap-2 py-2 px-3 pr-4 rounded text-sm bg-gray-800/50 backdrop-blur highlight-white/5 select-none duration-150 hover:bg-gray-700/50"
 			title="babak.bxf@gmail.com"
 			use:copy={"babak.bxf@gmail.com"}
 			on:click={() => toast.success("Copied to Clipboard!", { position: "bottom-right" })}
@@ -98,6 +98,11 @@
 			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-400" aria-hidden="true">
 				<path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
 			</svg>
+			<div
+				class="absolute -top-1 -right-2 | w-9 h-5 flex items-center justify-center | bg-gray-800 highlight-white/5 rounded text-gray-400 duration-300 ease-in-out sm:hide sm:scale-0 sm:group-hover:show sm:group-hover:scale-100"
+			>
+				<span class="text-2xs">copy</span>
+			</div>
 			Email
 		</button>
 		<Link href="https://t.me/babakfp">
