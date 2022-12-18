f698acc1d8b3937e6b70588aa7bc8f6e0a50d837
babakfp <44144724+babakfp@users.noreply.github.com>
2022-12-18 15:50:34 -0800

    responsive

--- src/lib/comps/Card.svelte
+++ src/lib/comps/Card.svelte
@@ -23,9 +23,9 @@
 			absolute right-0 -translate-x-4 | flex items-center justify-center | text-gray-400
 			{arrowCenter
 			? 'inset-y-center'
-			: 'top-0 translate-y-4 xl:-translate-y-full xl:group-hover:translate-y-4'}
-			xl:translate-x-full xl:scale-0
-			xl:duration-300 | xl:group-hover:scale-100 xl:group-hover:-translate-x-4
+			: 'top-0 translate-y-4 sm:-translate-y-full sm:group-hover:translate-y-4'}
+			sm:translate-x-full sm:scale-0
+			sm:duration-300 | sm:group-hover:scale-100 sm:group-hover:-translate-x-4
 		"
 	>
 		{#if isExternal}
