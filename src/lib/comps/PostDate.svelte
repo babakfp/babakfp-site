b764f429363d5ab33a41e106663ee4f6615d573b
babakfp <44144724+babakfp@users.noreply.github.com>
2022-12-18 11:00:59 -0800

    fixed responsive

--- src/lib/comps/PostDate.svelte
+++ src/lib/comps/PostDate.svelte
@@ -3,7 +3,7 @@
 	export let publishDate
 </script>
 
-<div class="flex items-center flex-wrap gap-4 | text-xs text-gray-400 font-mono">
+<div class="flex items-center flex-wrap gap-x-4 gap-y-2 | text-xs text-gray-400 font-mono">
 	<span>Publish date: <b>{publishDate}</b></span>
 	<span>Update date: <b>{updateDate}</b></span>
 </div>
