9b335d793f1079254ea8c5ab7109b7c930c5dafc
babakfp <44144724+babakfp@users.noreply.github.com>
2022-12-18 08:12:53 -0800

    changed name

--- src/lib/parts/Header/Header.svelte
+++ src/lib/parts/Header/Header.svelte
@@ -3,14 +3,14 @@
 	import MobileMenuToggle from "./Menu/MobileMenuToggle.svelte"
 	import DesktopMenu from "./Menu/DesktopMenu.svelte"
 	import SidebarToggle from "../Sidebar/SidebarToggle.svelte"
-	import Logo from "./Logo.svelte"
+	import Title from "./Title.svelte"
 </script>
 
 <header class="z-50 | sticky top-0 | h-header-height | bg-gray-800/95 backdrop-blur-lg shadow">
 	<div class="container px-0 | h-full | flex justify-between">
 		<div class="flex w-full">
 			<MobileMenuToggle />
-			<Logo />
+			<Title />
 		</div>
 
 		<SidebarToggle />
