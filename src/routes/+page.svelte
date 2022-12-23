972d65245ab9a5e38c09f8d944d9ec73eb3fbe5a
babakfp <44144724+babakfp@users.noreply.github.com>
2022-12-22 17:10:01 -0800

    Updated resume url v2.40 :)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,7 +54,7 @@
 	]
 
 	const resumeUrl =
-		"https://drive.google.com/file/d/1cdnOu2ptS_x-feqVRZEiQrelKclfMQqf/view?usp=sharing"
+		"https://drive.google.com/file/d/1DiGH4If-bb7MkIwaReqyTmY9dlUh6o8V/view?usp=sharing"
 	const githubReposUrl =
 		"https://github.com/babakfp?tab=repositories&q=&type=public&language=&sort=stargazers"
 	const dribbbleDesigns = "https://dribbble.com/babakfp"
